c3bb43239b66acbd4cddccdbaea3579368ec4e4e
Eng-Mohamed-Hesham <64752656+Eng-Mohamed-Hesham@users.noreply.github.com>
2021-06-11 14:54:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-function initMap() {
-    // The location of Cairo
-    var cairo = {lat: 30.044420, lng: 31.235712};
-    // The map, centered at Cairo
-    var map = new google.maps.Map(
-        document.querySelector(".map"), {zoom: 4, center: cairo});
-    // The marker, positioned at Cairo
-    var marker = new google.maps.Marker({position: cairo, map: map});
-  }
-  
-  
-  // Smooth scrolling
+ // Smooth scrolling
   $("#navbar a, .btn").on("click", function(event) {
   
     if(this.hash !== "") {
@@ -74,13 +63,9 @@ function validation(){
   var pass = document.getElementById('pass').value;
   var conpass = document.getElementById('conpass').value;
   var email = document.getElementById('email').value;
-  var mobile = document.getElementById('mobile').value;
-  
+  var mobile = document.getElementById('mobile').value;  
   var myform = document.getElementById('myform');
-   // to check if that field empty or not
-  
- 
- //  to accert a name of 3 letters at least
+    //  to accert a name of 3 letters at least
   if(user.length<3){
       alert("*first Name must be more than 2 letters*");
       return false;
@@ -90,28 +75,30 @@ function validation(){
     return false;
 }
   
-  
-  
   if(pass.search(/[ ]/)!=-1){
      alert("**must not include a space**");
   return false;
  }
+    
   // limit the lenght of the  pass
   if(pass.length != 8 ){
     alert("**please write a passward of 8 no long no less**");
       return false;
   }
+    
     // must enclud numbers
  if(pass.search(/[0-9]/)==-1){
       
       alert("must incluce at least a number");
       return false;
   }
+    
   //not to accept without upper characters
   if(pass.search(/[A-Z]/)==-1){
 alert("**must include at leasrt 1 upper character**");
       return false;
   }
+    
   // not to accept without lower characters
  if(pass.search(/[a-z]/)==-1){
       alert("**must include at least 1 lowwer character**");
@@ -123,6 +110,11 @@ alert("**must include at leasrt 1 upper character**");
          alert("**must include 1 special character**");
       return false;
      }
+    
+   if(pass.charAt(0)!==pass.charAt(0).toUpperCase()){
+    alert(**must start with an uppercase character);
+    return false;
+  }
   
   if(conpass != pass){
       alert("**passward isnot matches");
